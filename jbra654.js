8400388559f0625f087ab80a1fdbd0b98d247fbb
James Brady <jfbrady12345@gmail.com>
2021-10-04 00:16:18 +1300

    I have piss drops.

--- jbra654.js
+++ jbra654.js
@@ -1,15 +1,32 @@
-const getPieces = () => {
-    const secretPieces = fetch("http://localhost:5000/api/S");
-    const streamPromise = secretPieces.then((response) => response.text());
-    streamPromise.then((data) => { document.getElementById("secretPieces").innerText = data; });
-}
-
 const getIcon = () => {
     const fetchPromise = fetch("http://localhost:5000/api/GetIcon");
     const streamPromise = fetchPromise.then((response) => response.text());
-    streamPromise.then((data) => { document.getElementById("head").innerHTML += data; });
+    streamPromise.then((data) => {
+        document.getElementById("head").innerHTML += data;
+    });
 }
 
-getPieces();
-getIcon();
+const getPieces = () => {
+    const fetchPromise = fetch("http://localhost:5000/api/S");
+    const streamPromise = fetchPromise.then((response) => response.text());
+    streamPromise.then((data) => {
+        let pieces = JSON.parse(data);
+        pieces.forEach(displayPiss);
+        document.getElementById("array").innerText = pieces;
+    });
+}
 
+const displayPiss = (data) => {
+    let htmlString = ""; 
+    for (let i = 0; i < parseInt(data); i++) {
+        if (i % 2 == 0) {
+            htmlString += '<svg width="15" height="30"><use href="#sDrop" /></svg>';
+        } else {
+            htmlString += '<svg width="15" height="30" transform="rotate(180)"><use href="#sDrop" /></svg>';
+        };
+    };
+    document.getElementById("pissTable").innerHTML += `<tr><td>${data}</td><td>${htmlString}</td></tr>`;
+}
+
+getIcon();
+getPieces();
